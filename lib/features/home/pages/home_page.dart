af5bdb384bd1c502903c4b4a2156207d1e9bc5e0
sanam-tamang <snm.tmg7@gmail.com>
2025-03-07 10:37:25 +0545

    complete

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:day5/features/home/repo/weather_repo.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,75 +13,96 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SizedBox(
-          width: double.maxFinite,
-          height: double.maxFinite,
-          child: Stack(
-            children: [
-              Positioned.fill(
-                child: Image.asset('assets/images/bg.png', fit: BoxFit.cover),
-              ),
+        child: FutureBuilder(
+          future: WeatherRepo.getWeatherData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Text("Error: ${snapshot.error.toString()}");
+            }
 
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 24,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
+            final weatherModel = snapshot.data;
+
+            return SizedBox(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: Image.asset(
+                      'assets/images/bg.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 24,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Icon(Icons.location_on),
-                        Text("Kathmandu,Nepal"),
+                        Row(
+                          children: [
+                            Icon(Icons.location_on),
+                            Text(
+                              "${weatherModel?.region}, ${weatherModel?.country} ",
+                            ),
+                          ],
+                        ),
+
+                        SizedBox(height: 8),
+
+                        Text("Today, Sun"),
+
+                        // SizedBox(height: 12),
+                        SizedBox(
+                          width: 280,
+                          child: Stack(
+                            children: [
+                              Text(
+                                "${weatherModel?.celcious}",
+                                style: TextStyle(fontSize: 100),
+                              ),
+                              Positioned(
+                                right: -5,
+                                top: 30,
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Container(
+                                      height: 20,
+                                      width: 20,
+                                      decoration: BoxDecoration(
+                                        color: Colors.transparent,
+
+                                        border: Border.all(width: 3),
+
+                                        shape: BoxShape.circle,
+                                      ),
+                                    ),
+                                    Text(
+                                      "C",
+                                      style: TextStyle(
+                                        fontSize: 36,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
-
-                    SizedBox(height: 8),
-
-                    Text("Today, Sun"),
-
-                    // SizedBox(height: 12),
-                    SizedBox(
-                      width: 180,
-                      child: Stack(
-                        children: [
-                          Text("16", style: TextStyle(fontSize: 100)),
-                          Positioned(
-                            right: 0,
-                            top: 30,
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Container(
-                                  height: 20,
-                                  width: 20,
-                                  decoration: BoxDecoration(
-                                    color: Colors.transparent,
-
-                                    border: Border.all(width: 3),
-
-                                    shape: BoxShape.circle,
-                                  ),
-                                ),
-                                Text(
-                                  "C",
-                                  style: TextStyle(
-                                    fontSize: 36,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
--- lib/features/home/repo/weather_repo.dart
+++ lib/features/home/repo/weather_repo.dart
@@ -1,10 +1,22 @@
+import 'package:day5/features/home/model/weather.dart';
+import 'package:dio/dio.dart';
+
 const apiUrl =
     "https://api.weatherapi.com/v1/current.json?key=8587a2cd4567457d9f615102250703&q=KTM";
 
 class WeatherRepo {
-  Future<void> getWeatherData() async {
+  static Future<WeatherModel?> getWeatherData() async {
+    try {
+      final response = await Dio().get(apiUrl);
 
-    
-    
+      final data = response.data;
+
+      final weatherModel = WeatherModel.fromJson(data);
+
+      return weatherModel;
+    } on DioException catch (e) {
+      print(e.toString());
+      rethrow;
+    }
   }
 }
